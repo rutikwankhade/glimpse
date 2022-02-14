71197c844c6f1676f9a68117758477e19ba34233
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-15 01:31:23 +0530

    style: improve search bar nav

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,18 @@ import { useState, Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import Link from 'next/link'
 import Image from 'next/image'
+
 import searchIcon from '../assets/icons/bx-search.svg'
+
 
 export default function Home() {
 
-
   const [term, setTerm] = useState('')
-  const [response, setResponse] = useState([])
+  const [response, setResponse] = useState()
   const [timeoutId, setTimeoutId] = useState()
   const [isOpen, setIsOpen] = useState(false)
+
+
 
   const getBooks = async (term) => {
     const response = await fetch(
@@ -22,12 +25,16 @@ export default function Home() {
     setResponse(data.items)
   };
 
+
+
   const handleInputChange = (query) => {
     clearTimeout(timeoutId)
     setTerm(query);
     const timeout = setTimeout(() => getBooks(term), 500)
     setTimeoutId(timeout)
   }
+
+
 
 
   return (
@@ -38,7 +45,6 @@ export default function Home() {
       </Head>
 
       <main className="">
-
 
         <Transition appear show={isOpen}>
 
@@ -78,12 +84,10 @@ export default function Home() {
                 leaveTo="opacity-0 scale-95"
               >
 
-
                 <div className=" inline-block w-full max-w-5xl  p-10 my-8  text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
 
                   <div className="flex p-4 items-center w-10/12  m-2 mx-auto border bg-gray-50 shadow-gray-50 shadow-xl rounded-xl">
-                    <Image src={searchIcon} />
-
+                    <Image className="opacity-50" src={searchIcon} />
                     <input
                       placeholder="Search book by name or author"
                       className=" w-full ml-2 bg-gray-50 flex text-xl text-gray-600  outline-none"
@@ -94,49 +98,44 @@ export default function Home() {
 
                   <div className=" overflow-y-scroll h-96 ">
                     <div className="flex flex-row flex-wrap justify-center ">
-                      {response.map(book => {
+                      {response && response.map(book => {
                         return (
                           <Link href={`/books/${book.id}`} >
                             <div key={book.accessInfo.id}
-                              className="cursor-pointer hover:bg-gray-50 flex flex-col border cursor m-2 p-4 w-1/4 items-center h-auto shadow-sm rounded-xl "
-                            >
+                              className="cursor-pointer hover:bg-gray-50 flex flex-col border cursor m-2 p-4 w-1/4 items-center h-auto shadow-sm rounded-xl ">
                               <img src={book.volumeInfo.imageLinks.thumbnail} className="rounded-xl w-24  shadow-xl shadow-slate-200 border" />
                               <h1 className="text-xl text-center text-gray-700 font-semibold mt-4">{book.volumeInfo.title}</h1>
                             </div>
                           </Link>
                         )
                       })}
+
                     </div>
                   </div>
-
 
                 </div>
               </Transition.Child>
 
 
-
-
-
               {/* <button className="border rounded-full p-2 text-2xl h-10 w-10" onClick={() => setIsOpen(false)}>X</button> */}
 
-
-
-
             </div>
-
 
           </Dialog>
 
         </Transition>
 
         <div className="">
-          <button
+
+          <div
             onClick={() => setIsOpen(true)}
-            className="text-3xl bg-yellow-200 p-2 m-2 rounded-full"
-          >🔍</button>
+
+            className="flex bg-white  bg-gray-50  text-lg my-4 text-gray-400  outline-none cursor-pointer p-2 items-center w-1/3 m-2 ml-auto mr-10 border bg-gray-50 shadow-gray-50 shadow-xl rounded-xl">
+            <Image className="opacity-50" src={searchIcon} />
+            <span className="ml-2">Search book by name or author</span>
 
 
-
+          </div>
 
         </div>
 
--- utils/utils.js
+++ utils/utils.js
@@ -1,13 +1,10 @@
-export const colors = [
-    'bg-gray-100', 'bg-red-100', 'bg-pink-100', 'bg-orange-200', 'bg-yellow-100','bg-green-100', 'bg-teal-100', 'bg-cyan-100', 'bg-blue-100', 'bg-indigo-100', 'bg-violet-100', 'bg-purple-100', 'bg-fuchsia-100', 'bg-rose-100'
-];
+
 
 export const getRandomColor = () => {
     const random = Math.random();
     const color = {
         p1: "hsl(" + random * 360 + ", 100%, 95%)",
-                p2: "hsl(" + random * 360 + ", 100%, 91%)"
-
+        p2: "hsl(" + random * 360 + ", 100%, 91%)"
     }
-  return color;
+    return color;
 }
