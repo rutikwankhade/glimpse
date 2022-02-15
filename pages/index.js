74de888d033f6bcbe48762e0b992b907e042df02
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-15 14:33:13 +0530

    feat: add categories in search modal

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { useState, Fragment } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
 import Link from 'next/link'
 import Image from 'next/image'
+import {categories} from '../services/books'
 
 import searchIcon from '../assets/icons/bx-search.svg'
 import cover from '../assets/images/cover.jpg'
@@ -115,6 +116,17 @@ export default function Home() {
                           </Link>
                         )
                       })}
+
+                      {
+                        categories.map(category => {
+                          return <div className="flex flex-col items-center px-10 py-6 shadow-sm hover:bg-gray-50 cursor-pointer border m-4 rounded-xl">
+                            <span className="text-5xl m-2">
+                              { category.emoji}
+                            </span>
+                            <h2 className="text-xl capitalize">{category.name}</h2>
+                          </div>
+                        })
+                      }
 
                     </div>
                   </div>
---
+++ services/books.js
@@ -0,0 +1,42 @@
+export const categories = [
+    {
+        emoji: '✍',
+        name: 'biographies',
+     
+    },
+    {
+        emoji: '💡',
+        name: 'business',
+       
+    },
+    {
+        emoji: '🌇',
+        name: 'fiction',
+       
+    },
+    {
+        emoji: '📚',
+        name: 'literature',
+     
+    },
+    {
+        emoji: '🌆',
+        name: 'non fiction',
+      
+    },
+    {
+        emoji: '💖',
+        name: 'romance',
+       
+    },
+    {
+        emoji: '🔬',
+        name: 'science',
+       
+    },
+    {
+        emoji: '🌼',
+        name: 'spirituality',
+       
+    },
+]
