78027d559b85b397c2dbb326c7e70bb97ccca8eb
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-15 15:30:57 +0530

    feat:get books by category

--- pages/books/category/[category].js
+++ pages/books/category/[category].js
@@ -1,8 +1,43 @@
+import { useState, useEffect } from 'react'
+import { categories } from '../../../services/books'
+
 const Category = (params) => {
+    const [response, setResponse] = useState()
+    const [emoji, setEmoji] = useState()
+
+
+    const getBooks = async (term) => {
+        const response = await fetch(
+            `https://www.googleapis.com/books/v1/volumes?q=${term}`
+        );
+        const data = await response.json();
+        console.log(data)
+        setResponse(data.items)
+    };
+
+
+
+    useEffect(() => {
+        const res = categories.find(obj => {
+            return obj.name === params.params.category
+        })
+
+        setEmoji(res.emoji)
+
+
+        getBooks(`subject:${params.params.category}&startIndex=0&maxResults=20`)
+
+    }, [])
+
+
     return (
         <div className="bg-gray-50 w-full">
-            category page
-            {params.params.category}
+            <div className="flex items-center text-5xl m-4">
+                <span className="border rounded-xl p-4 h-24 w-24 bg-white">{emoji}</span>
+
+                <h1 className=" font-bold ml-4 capitalize text-gray-800">{params.params.category}</h1>
+            </div>
+
         </div>
     );
 }
