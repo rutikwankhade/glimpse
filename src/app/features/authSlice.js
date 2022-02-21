dc26f3446327f80bf8902c260b16d08e38fcc9cf
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-21 23:44:28 +0530

    :sparkles: add login reducer

--- src/app/features/authSlice.js
+++ src/app/features/authSlice.js
@@ -32,6 +32,37 @@ export const registerUser = createAsyncThunk(
 
 
 
+
+export const loginUser = createAsyncThunk(
+  "api/auth/login",
+  async ({ email, password }) => {
+      
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const body = JSON.stringify({ email, password })
+    console.log(body)
+
+    try {
+      const res = await axios.post('https://glimpsecommunity.herokuapp.com/api/auth/signup', body, config);
+
+      if (res.data.success) {
+        return res.data;
+      }
+
+
+    } catch (err) {
+
+      console.log("Couldn't login", err);
+    }
+  }
+);
+
+
 export const authSlice = createSlice({
   name: "user",
   initialState: {
@@ -48,6 +79,15 @@ export const authSlice = createSlice({
       state.status = "loading";
     },
     [registerUser.fulfilled]: (state, action) => {
+      state.status = "success";
+      console.log(action.payload)
+      state.token = action.payload;
+
+    },
+    [loginUser.pending]: (state, action) => {
+      state.status = "loading";
+    },
+    [loginUser.fulfilled]: (state, action) => {
       state.status = "success";
       console.log(action.payload)
       state.token = action.payload;
