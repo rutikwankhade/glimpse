ae0bff13f2c681c8696ca5c89b689be3a58d558a
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-22 11:54:22 +0530

    :sparkles: add use form hook

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+
 import { registerUser } from "../app/features/authSlice";
 
 const SignUp = () => {
 
- const dispatch = useDispatch()
+    const dispatch = useDispatch()
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
 
 
     const handleUserLogin = (e) => {
@@ -17,48 +21,57 @@ const SignUp = () => {
         dispatch(registerUser({
             username: "rutik24",
             email: "rutik24@gmail.com",
-            password:"12345"
+            password: "12345"
         }))
 
-    
-}
+
+
+    }
+
+    const onSubmit = (data) => {
+        console.log(data);
+    }
 
 
     return (
         <div className="w-full">
             <div className="flex items-center justify-center">
 
-                <form
+                <form onSubmit={handleSubmit(onSubmit)}
                     className=" bg-white  p-10  flex flex-col border m-20  rounded-md">
                     <h1 className="text-2xl font-semibold mb-6">Sign up</h1>
 
 
                     <input
-                     
                         type="text"
                         placeholder="name"
                         className="text-lg m-2 bg-gray-100 py-2 px-4 w-full rounded"
+                        {...register("username")}
+
+
                     />
 
+
                     <input
-                     
                         type="email"
                         placeholder="Email"
                         className="text-lg m-2 bg-gray-100 px-4 py-2 w-full rounded"
+                        {...register("email")}
+
                     />
 
                     <input
-                     
                         type="password"
                         placeholder="Password"
                         className="text-lg m-2 bg-gray-100 px-4 py-2 w-full rounded"
+                        {...register("password", { required: true, min: 8 })}
+
                     />
 
 
-                    <button
-                                                onClick={(e)=>handleUserLogin(e)}
 
-                        // type="submit"
+                    <button
+                        type="submit"
                         className="text-lg  w-full m-2 bg-gray-700  font-semibold text-white px-6 py-2 rounded">Sign up
                     </button>
 
