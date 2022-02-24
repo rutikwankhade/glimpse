d84a226a51a279b013550d559e3823364be28c4f
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-24 15:20:58 +0530

    :sparkles:update header and hero section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,56 @@
 import Link from "next/link";
 import avatarIcon from '../assets/images/avatar.png'
 import Image from "next/image";
-import {  useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-        const { user} = useSelector((state) => state.auth);
+    const { user, isAuthenticated } = useSelector((state) => state.auth);
 
 
     return (
-        <div className="border flex items-center w-full p-4 px-20">
-            <Link href="/">
-                <h1 className="text-xl font-semibold italic">Glimpse</h1>
+        <div className=" ">
+            <div className="flex items-center  p-4 w-10/12 mx-auto">
+
+                <Link href="/">
+                    <a className="text-xl font-bold font-serif italic">Glimpse</a>
+                </Link>
+
+                {isAuthenticated
+                    ?
+                    <div className="flex items-center ml-auto mr-10">
+                        <span className="text-xl px-8 mr-4 font-semibold">{user?.username} </span>
+                        <Image src={avatarIcon} className=" px-4 border-white border-2 rounded-full"
+                            height="40"
+                            width="40"
+                        />
+
+                    </div>
+                    :
+                    <div className="flex items-center ml-auto mr-10">
+                        <Link href="/login">
+                            <button className="bg-white text-xl font-bold p-2 rounded-full px-6 ml-auto mr-4">                            Log In
+                            </button>
+                        </Link>
+                    </div>
+                }
 
 
-            </Link>
+
+
+            </div>
+
 
             <div className="mx-auto">
-                <Link href="/bookclub" >
+                {/* <Link href="/bookclub" >
                     <a className="text-xl flex rounded align-baseline font-semibold font-sans">
                         <span>📖</span><span >Books</span>
                     </a>
-                </Link>
+                </Link> */}
 
             </div>
 
-            <div className="flex items-center mr-10">
-                <span className="text-xl px-8 mr-4 font-semibold">{user?.username} </span> 
-                <Image src={avatarIcon} className=" px-4 rounded-full"
-                    height="40"
-                    width="40"
-                />
 
-            </div>
-           
+
 
         </div>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,30 @@
+import Image from 'next/image';
+import hero1 from '../assets/images/hero-1.png'
+import Link from 'next/link';
 const Home = () => {
 
   return (
     <div className="w-full">
 
-      <div className="m-10 flex flex-col items-center">
-        <h1 className=" text-center text-6xl font-bold">Discover books and movies</h1>
-        
-      
+      <div className="m-10 flex flex-row w-10/12 mx-auto">
+
+        <div className=" w-1/2">
+          <h1 className="  text-6xl font-serif m-10 font-bold leading-2">Discover good books from the readers just like you</h1>
+
+          <Link href="/signup">
+            <button className="bg-white text-2xl font-bold p-2 rounded-full px-6 mx-8">Let's get started</button>
+          </Link>
+        </div>
+
+
+
+
+        <div className="w-1/2">
+          <Image src={hero1} />
+
+        </div>
+
+
       </div>
 
     </div>
