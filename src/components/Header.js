d693a16ef1e8863bad974c4815a9f9ec5f5df59b
rutik wankhade <rutikwankhade2@gmail.com>
2022-02-25 23:32:50 +0530

    :recycle: refactor header and make search bara seperate component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,52 @@
 import Link from "next/link";
-import avatarIcon from '../assets/images/avatar.png'
 import Image from "next/image";
 import { useSelector } from "react-redux";
+
+import SearchBar from './Searchbar'
+import avatarIcon from '../assets/images/avatar.png'
 
 const Header = () => {
     const { user, isAuthenticated } = useSelector((state) => state.auth);
 
 
     return (
-        <div className=" ">
-            <div className="flex items-center  p-4 w-10/12 mx-auto">
+        <div>
+            <div className="flex items-center justify-evenly  p-4 w-10/12 mx-auto">
 
                 <Link href="/">
                     <a className="text-xl font-bold font-serif italic">Glimpse</a>
                 </Link>
 
-                {isAuthenticated
-                    ?
-                    <div className="flex items-center ml-auto mr-10">
-                        <span className="text-xl px-8 mr-4 font-semibold">{user?.username} </span>
-                       
-                      <Link href={`/profile/${user._id}`} >
-                        <Image src={avatarIcon} className=" px-4 border-white border-2 rounded-full"
-                            height="40"
-                            width="40"
-                            />
-                           
-</Link>
-                    </div>
-                    :
-                    <div className="flex items-center ml-auto mr-10">
-                        <Link href="/login">
-                            <button className="bg-white text-xl font-bold p-2 rounded-full px-6 ml-auto mr-4">                            Log In
-                            </button>
-                        </Link>
-                    </div>
-                }
+                <SearchBar />
+
+                <div className="ml-auto mr-4">
+                    {isAuthenticated
+                        ?
+                        <div className="flex items-center ">
+                            <span className="text-xl px-8 mr-4 font-semibold">{user?.username} </span>
+
+                            <Link href={`/profile/${user._id}`} >
+                                <Image src={avatarIcon} className="h-10 w-10 px-4 border-white border-2 rounded-full"
+                                    height="80"
+                                    width="80"
+                                />
+
+                            </Link>
+                        </div>
+                        :
+                        <div className="w-max ">
+                            <Link href="/login" >
+                                <a className="bg-white text-xl font-bold p-2 rounded-full ">Log In</a>
+                            </Link>
+                        </div>
+                    }
+
+                </div>
 
 
 
 
             </div>
-
-
-            <div className="mx-auto">
-                {/* <Link href="/bookclub" >
-                    <a className="text-xl flex rounded align-baseline font-semibold font-sans">
-                        <span>📖</span><span >Books</span>
-                    </a>
-                </Link> */}
-
-            </div>
-
-
-
 
         </div>
     );
