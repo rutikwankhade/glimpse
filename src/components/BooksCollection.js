1c4f369463dca31f7042ad043668640d9aa973d3
rutik wankhade <rutikwankhade2@gmail.com>
2022-03-01 01:59:53 +0530

    :sparkles: add stats on profile and manage empty states

--- src/components/BooksCollection.js
+++ src/components/BooksCollection.js
@@ -8,7 +8,7 @@ const BooksCollection = ({ collection, posts }) => {
             <Tab.Group>
 
                 <Tab.List className="w-full justify-center  bg-gray-50 p-2 border rounded-b-xl flex flex-row items-center ">
-                                        <Tab className="rounded-xl shadow-lg px-4 m-2 bg-white text-lg font-semibold p-2">Stats</Tab>
+                    <Tab className="rounded-xl shadow-lg px-4 m-2 bg-white text-lg font-semibold p-2">🚀 Stats</Tab>
 
                     <Tab className="rounded-xl shadow-lg px-4 m-2 bg-white text-lg font-semibold p-2">📖 Currently Reading</Tab>
                     <Tab className=" rounded-xl shadow-lg px-4 m-2 bg-white text-lg font-semibold p-2">🔖 Want to Read</Tab>
@@ -19,44 +19,93 @@ const BooksCollection = ({ collection, posts }) => {
 
                 <Tab.Panels>
 
-                  <Tab.Panel>
-                        <div className="flex flex-wrap">
-                            <div>
-                               <span> {collection.filter(book => book.status === "Want to Read").length}</span>
+                    <Tab.Panel >
+                        <div className="flex mx-auto justify-center w-10/12 p-4">
+                            <div className="border bg-purple-100 rounded-xl shadow w-1/4 p-4 m-4 flex flex-col text-center">
+                                <span className="text-5xl font-semibold m-2"> {collection?.filter(book => book.status === "Want to Read").length}</span>
+                                <span className="text-xl text-gray-600">Want to Read</span>
+                            </div>
+
+                            <div className="border bg-green-100 rounded-xl shadow w-1/4 p-4 m-4 flex flex-col text-center">
+                                <span className="text-5xl font-semibold m-2"> {collection?.filter(book => book.status === "Read").length}</span>
+                                <span className="text-xl text-gray-600">Completed</span>
+                            </div>
+
+                            <div className="border bg-yellow-100 rounded-xl shadow w-1/4 p-4 m-4 flex flex-col text-center">
+                                <span className="text-5xl font-semibold m-2"> {posts && posts.length}</span>
+                                <span className="text-xl text-gray-600">Glimpse shared</span>
                             </div>
                         </div>
                     </Tab.Panel>
 
 
                     <Tab.Panel>
-                        <div className="flex flex-wrap">
+                                 <div className="">
                             {
-                                collection?.filter(book => book.status === "Currently reading").map(book => {
-                                    return <BookCard book={book} />  
-                                })
+                                (collection && collection?.filter(book => book.status === "Currently Reading").length) ?
+                                    <div className="flex flex-wrap">
+                                        {
+                                            collection?.filter(book => book.status === "Read").map(book => {
+                                                return <BookCard book={book} />
+                                            })
+                                        }
+
+                                    </div> :
+
+                                    <div className="flex flex-col mx-auto text-center my-10 w-max p-8 rounded-xl bg-yellow-50">
+                                        <h1 className="text-2xl font-semibold">Nothing on your shelf right now.</h1>
+                                        <h2 className="text-xl">This could be the right time to pick a new book.</h2>
+                                    </div>
+
                             }
+
                         </div>
                     </Tab.Panel>
 
 
                     <Tab.Panel>
-                        <div className="flex flex-wrap">
+                        <div className="">
                             {
-                                collection?.filter(book => book.status === "Want to Read").map(book => {
-                                    return <BookCard book={book} />  
-                                })
+                                (collection && collection?.filter(book => book.status === "Want to Read").length) ?
+                                    <div className="flex flex-wrap">
+                                        {
+                                            collection?.filter(book => book.status === "Read").map(book => {
+                                                return <BookCard book={book} />
+                                            })
+                                        }
+
+                                    </div> :
+                                    <div className="flex flex-col mx-auto text-center my-10 w-max p-8 rounded-xl bg-yellow-50">
+                                        <h1 className="text-2xl font-semibold">No books on your list yet.</h1>
+                                        <h2 className="text-xl">Follow other readers and discover new books from your feed.</h2>
+                                    </div>
+
                             }
+
                         </div>
                     </Tab.Panel>
 
 
                     <Tab.Panel>
-                        <div className="flex flex-wrap">
+                        <div className="">
                             {
-                                collection?.filter(book => book.status === "Read").map(book => {
-                                    return <BookCard book={book} />   
-                                })
+                                (collection && collection?.filter(book => book.status === "Read").length) ?
+                                    <div className="flex flex-wrap">
+                                        {
+                                            collection?.filter(book => book.status === "Read").map(book => {
+                                                return <BookCard book={book} />
+                                            })
+                                        }
+
+                                    </div> :
+
+                                    <div className="flex flex-col mx-auto text-center my-10 w-max p-8 rounded-xl bg-yellow-50">
+                                        <h1 className="text-2xl font-semibold">You haven't marked any book as read yet.</h1>
+                                        <h2 className="text-xl">This is your sign to stat reading!</h2>
+                                    </div>
+
                             }
+
                         </div>
                     </Tab.Panel>
 
